374c3e40bf35b9491e7a6c939a87f21cf8847fdb
Eleanore Campbell <eleanorecampbell@Eleanortes-MBP.lan>
2019-06-11 18:35:51 +0200

    regions_shp to create shape objs in globalEnv, join_rgns_info to add bhi/helcom/ices IDs to a dataset with lat/lon or helcomID+country; deleting plotting stuff for now

--- R/spatial.R
+++ R/spatial.R
@@ -4,106 +4,105 @@ library(rgdal)
 library(sp)
 library(sf)
 library(raster)
-library(fasterize)
-library(rgeos)
-library(maptools)
-library(ncdf4)
-library(colorRamps)
-library(paletteer)
-library(parallel)
-library(RQGIS)
-library(tibble)
+# library(fasterize)
+# library(rgeos)
+# library(maptools)
+# library(ncdf4)
+# library(colorRamps)
+# library(parallel)
+# library(RQGIS)
+# library(tibble)
 
 ## Functions
 
-subset_to_bhi_rgns <- function(){
+#' create regions shape objects
+#' 
+#' @param sp_dir the file location of the shapefiles folders; the directory should contain afolder for each shapefile
+#' @param foldernames the folders containing respective shapefiles
+#'
+#' @return defines spatial objects: bhi_rgns, helcom_subbasins, nuts2_rgns, ices_rgns, baltic_mpas in the global environment
+
+regions_shape <- function(sp_dir = file.path(dir_B, "Spatial"),
+                          foldernames = c("bhi_shapefile", # "helcom_subbasins_shapefile",
+                                          "nuts2_shapefile", "ices_rgn_shapefile", 
+                                          "mpas_shapefile")){
   
+  for(i in foldernames){
+    
+    if(file.exists(file.path(sp_dir, i))){
+      shp_path <- list.files(file.path(sp_dir, i), full.names = TRUE, 
+                             pattern = "[A-Za-z0-9_]+.shp$") # assumes 1 shapefile per folder
+      shp <- sf::st_read(dsn = file.path(sp_dir, i), 
+                         layer = str_extract(basename(shp_path), pattern = "[A-Za-z0-9_]+"))
+      
+      name_obj <- paste0(str_extract(i, pattern = "[a-zA-Z0-9]+"), "_rgns_shp")
+      cat("creating object in global environment:", name_obj)
+      assign(name_obj, shp, envir = .GlobalEnv) # creates object in global environment!
+      
+    } else {
+      warning(paste(i, "not found, check it exists in spatial directory", sp_dir))
+    }
+  }
 }
 
 
-# loc = "C:/Users/jgrif/Documents/StockholmUnivPostDoc/BalticHealthIndex/BHI_r/shapefiles/BHI_shapefile_projected"
-plot_bhi_with_nuts_rgns <- function(shp_loc){
+#' add information about regions to dataset
+#'
+#' @param dataset the dataset to add spatial information to (i.e bhi, helcom, and ices regions); ideally with latitute/longitude
+#' @param helcomID_col string naming the column in the dataset with HELCOM IDs
+#' @param country_col string indicating name of column in the dataset containing country names
+#' @param latlon_vars a two element character-vector with names of latitude and longitude columns, values of which can be numeric or character
+#' @param rgn_shps_loc the location of the shapefiles with information BHI, HELCOM, and ICES regions
+#'
+#' @return
+
+join_rgns_info <- function(dataset, helcomID_col = "helcom_id", country_col = "country",
+                           latlon_vars = c("^lat", "^lon"), 
+                           rgn_shps_loc = file.path(dir_B, "Spatial")){
   
-  ## read shapefiles from location specified by shapefile location param 'shp_loc'
-  bhi_shp <- st_read(shp_loc, "BHI_shapefile_projected.shp") %>% 
-    sf::st_transform("+proj=longlat +init=epsg:4326")
-  plot(st_geometry(bhi_shp))
+  colnames(dataset) <- stringr::str_to_lower(names(dataset))
   
-  bhi_shp_buffer <- st_read(shp_loc, "BHI_shapefile_25km_buffer_projected.shp") %>% 
-    sf::st_transform("+proj=longlat +init=epsg:4326")
-  plot(st_geometry(bhi_shp_buffer))
+  lat <- grep(colnames(dataset), pattern = latlon_vars[1], value = TRUE)
+  lon <- grep(colnames(dataset), pattern = latlon_vars[2], value = TRUE)
+  message(sprintf("using latitude longitude colnames identified: '%s' and '%s' respectively", lat, lon))
   
-  nuts2 <- st_read(shp_loc, "NUTS2006_Level_2_reprojected.shp") %>% 
-    sf::st_transform("+proj=longlat +init=epsg:4326")
-  plot(st_geometry(nuts2))
-  
-  nuts3 <- st_read(shp_loc, "NUTS2006_Level_3_reprojected.shp") %>% 
-    sf::st_transform("+proj=longlat +init=epsg:4326")
-  plot(st_geometry(nuts3))
+  ## call regions_shape function also in this script, to define shapefiles needed in global env
+  regions_shape(sp_dir = rgn_shps_loc, foldernames = c("bhi_shapefile", "ices_rgn_shapefile"))
   
   
-  
-  
+  if(length(lat) == 1 & length(lon) == 1 & !any(is.na(latlon_vars))){
+    ## if there is lat lon information, use sf::st_join with shapefiles to assign regions
+    data_sf <- st_as_sf(dataset, coords = c(lon, lat), crs = 4326, agr = "constant")
+       
+    if(exists("bhi_rgns_shp", envir = .GlobalEnv)){
+      data_sf <- st_join(data_sf, bhi_rgns_shp) 
+    } else { message("BHI regions w/ HELCOM ID shapefile not in global environment")}
+    
+    if(exists("ices_rgns_shp", envir = .GlobalEnv)){
+      data_sf <- st_join(data_sf, ices_rgns_shp)
+    } else { message("ICES regions shapefile not in global environment")}
+    
+    data_rgns_joined <- data_sf %>% 
+      as_tibble() %>% 
+      select(-geometry, -Area_km2)
+    
+  } else {
+    ## if no lat lon, need to use country or subbasin or other information... 
+    bhi_rgns_df <- bhi_rgns_shp %>% 
+      as_tibble() %>% 
+      select(rgn_nam, rgn_key, HELCOM_ID, BHI_ID)
+    colnames(bhi_rgns_df) <- stringr::str_to_lower(colnames(bhi_rgns_df))
+
+    ## join by helcom_id then use combination of helcom_id and country name/key to determine bhi_id
+    data_rgns_joined <- dplyr::left_join(
+      dataset, bhi_rgns_df,
+      by = c(helcomID_col = "helcom_id", country_col = "rgn_nam"))
+  }
+
+  return(data_rgns_joined)
 }
 
 
-##---------------------------------------##
-## PLOT BHI REGIONS and NUTS3 REGIONS ##
-##---------------------------------------##
-
-## get centroids of polygons for plotting labels
-centroids_bhi <- getSpPPolygonsLabptSlots(BHIshp2)%>% data.frame()%>%
-  dplyr::rename(lon=X1, lat=X2)%>%
-  mutate(BHI_ID = BHIshp2@data$BHI_ID)
-
-centroids_nuts3 =getSpPPolygonsLabptSlots(NUTS3_2)%>% data.frame()%>%
-  dplyr::rename(lon=X1, lat=X2)%>%
-  mutate(NUTS3_ID = NUTS3_2@data$NUTS_ID)
-
-## plot NUTS3 and shapefile with buffer
-png(file.path(dir_prep,"BHI_regions_NUTS3_withbuffer_plot.png"),
-    width = 210, height=297, units="mm",res=300 )
-plot(BHIshp_buffer2, col="red")
-plot(BHIshp2, add=TRUE)
-plot(NUTS3_2, add=TRUE)
-text(centroids_nuts3$lon,centroids_nuts3$lat, labels=centroids_nuts3$NUTS3_ID, col="black",cex=.3)
-text(centroids_bhi$lon, centroids_bhi$lat, labels=centroids_bhi$BHI_ID, col="red",cex=.5)
-dev.off()
-
-## plot NUTS3 and shapefile without buffer
-png(file.path(dir_prep,"BHI_regions_NUTS3_plot.png"),
-    width = 210, height=297, units="mm",res=300 )
-plot(BHIshp2,col="red")
-plot(NUTS3_2, add=TRUE)
-text(centroids_nuts3$lon,centroids_nuts3$lat, labels=centroids_nuts3$NUTS3_ID, col="black",cex=.3)
-text(centroids_bhi$lon, centroids_bhi$lat, labels=centroids_bhi$BHI_ID, col="black",cex=.5)
-dev.off()
-
-
-##---------------------------------------##
-## PLOT BHI REGIONS and NUTS2 REGIONS ##
-##---------------------------------------##
-
-## get centroids of polygons for plotting labels
-centroids_nuts2 =getSpPPolygonsLabptSlots(NUTS2_2)%>% data.frame()%>%
-  dplyr::rename(lon=X1, lat=X2)%>%
-  mutate(NUTS2_ID = NUTS2_2@data$NUTS_ID)
-
-## plot NUTS2 and shapefile with buffer
-png(file.path(dir_prep,"BHI_regions_NUTS2_withbuffer_plot.png"),
-    width = 210, height=297, units="mm",res=300 )
-plot(BHIshp_buffer2, col="red")
-plot(BHIshp2, add=TRUE)
-plot(NUTS2_2, add=TRUE)
-text(centroids_nuts2$lon,centroids_nuts2$lat, labels=centroids_nuts2$NUTS2_ID, col="black",cex=.3)
-text(centroids_bhi$lon, centroids_bhi$lat, labels=centroids_bhi$BHI_ID, col="red",cex=.5)
-dev.off()
-
-## plot NUTS2 and shapefile without buffer
-png(file.path(dir_prep,"BHI_regions_NUTS2_plot.png"),
-    width = 210, height=297, units="mm",res=300 )
-plot(BHIshp2,col="red")
-plot(NUTS2_2, add=TRUE)
-text(centroids_nuts2$lon,centroids_nuts2$lat, labels=centroids_nuts2$NUTS2_ID, col="black",cex=.3)
-text(centroids_bhi$lon, centroids_bhi$lat, labels=centroids_bhi$BHI_ID, col="black",cex=.5)
-dev.off()
+# calc_means_monthly <- function(){}
+# subset_to_bhi_rgns <- function(){}
+# make_layer_map <- function(){}
