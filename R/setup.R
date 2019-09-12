6d191922bd73b83c61dae53be214faee1c5e81ca
Eleanore Campbell <eleanorecampbell@Eleanortes-MacBook-Pro.local>
2019-09-12 10:32:24 +0200

    rename common.R to setup.R

--- R/setup.R
+++ R/setup.R
@@ -17,4 +17,11 @@ if(Sys.info()[["sysname"]] != "Linux" & !file.exists(dir_B)){
         sprintf("Do you need to mount the BHI server: %s?", dir_B))
 }
 
-assessment_yr <- "v2019"
+bhi_postgres_db <- "make connection to postgres+postgis database; for saving spatial data and other raw/intermediate data..."
+
+
+## General ----
+assess_year <- 2019 # CHANGE BHI ASSESSMENT YEAR HERE!
+version_year <- paste0("v", assess_year)
+rgn_ids_vec <- 1:42
+subbasin_ids_vec <- 501:517
