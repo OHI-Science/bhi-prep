605f1438bdeb81cf482959d0f2c4d92f677edc1a
eleanorecc <eleanorecampbell@ucsb.edu>
2020-01-22 10:31:56 +0100

    some small edits/additions

--- R/setup.R
+++ R/setup.R
@@ -6,13 +6,14 @@ library(tidyr)
 library(stringr)
 library(knitr)
 library(ggplot2)
+library(zoo)
 
 ## Directories
 ## CHANGE BHI PREP DIRECTORY HERE!
 dir_prep <- here::here()
 dir_layers <- file.path(dir_prep, "layers")
 dir_spatial <- file.path(dir_prep, "spatial") # spatial folder of bhi repo
-dir_bhi <- stringr::str_replace(dir_prep, pattern = "bhi-prep$", "bhi")
+dir_assess <- stringr::str_replace(dir_prep, pattern = "bhi-prep$", "bhi")
 dir_B <- file.path(c("Darwin" = "/Volumes/BHI_share", # "Windows" = ?
                      "Linux" = "/home/shares/ohi")[[ Sys.info()[["sysname"]] ]], "BHI 2.0") # CHANGE MAIN AUX BHI DIRECTORY HERE!
 if(Sys.info()[["sysname"]] != "Linux" & !file.exists(dir_B)){ 
@@ -27,5 +28,6 @@ bhi_postgres_db <- "make connection to postgres+postgis database; for saving spa
 ## General ----
 assess_year <- 2019 # CHANGE BHI ASSESSMENT YEAR HERE!
 version_year <- paste0("v", assess_year)
+bhi_version <- "v2019"
 rgn_ids_vec <- 1:42
 subbasin_ids_vec <- 501:517
