0167e027025ca21872bd93ca9b52e017a8e1ffd8
eleanorecc <eleanoreccampbell@gmail.com>
2020-01-23 14:17:17 +0100

    change filepath to point to new data location

--- R/setup.R
+++ R/setup.R
@@ -14,12 +14,16 @@ dir_prep <- here::here()
 dir_layers <- file.path(dir_prep, "layers")
 dir_spatial <- file.path(dir_prep, "spatial") # spatial folder of bhi repo
 dir_assess <- stringr::str_replace(dir_prep, pattern = "bhi-prep$", "bhi")
-dir_B <- file.path(c("Darwin" = "/Volumes/BHI_share", # "Windows" = ?
-                     "Linux" = "/home/shares/ohi")[[ Sys.info()[["sysname"]] ]], "BHI 2.0") # CHANGE MAIN AUX BHI DIRECTORY HERE!
-if(Sys.info()[["sysname"]] != "Linux" & !file.exists(dir_B)){ 
+
+# CHANGE MAIN AUX BHI DIRECTORY HERE!
+dir_B <- "/mnt/data/ellie/bhi_share/BHI 2.0"
+if(!file.exists(dir_B)){ 
   ## warning if BHI internal, shared directory doesn't exist
-  paste("The BHI directory dir_share set in R/common.R does not exist.",
-        sprintf("Do you need to mount the BHI server: %s?", dir_B))
+  sprintf(
+    "The BHI directory dir_share set in R/common.R does not exist.\n
+    Do you need to mount the BHI server: %s?", 
+    dir_B
+  )
 }
 
 bhi_postgres_db <- "make connection to postgres+postgis database; for saving spatial data and other raw/intermediate data..."
