ab00e38afa9729622d88568ae5c48e729d495b70
eleanorecc <eleanoreccampbell@gmail.com>
2020-03-17 18:59:15 +0100

    use st_drop_geometry rather than try to deselect

--- R/spatial.R
+++ R/spatial.R
@@ -143,7 +143,9 @@ join_rgns_info <- function(dataset, helcomID_col = "helcom_id", country_col = "c
     if(!return_spatial){
       data_rgns_joined <- data_rgns_joined %>% 
         as_tibble() %>% 
-        select(-geometry, -Area_km2_BHI, -Area_km2_ICES) 
+        st_drop_geometry() %>% 
+        select(-Area_km2_BHI, -Area_km2_ICES) %>% 
+        mutate(Subbasin = as.character(Subbasin), HELCOM_ID = as.character(HELCOM_ID))
     }
   
   ## country/subbasin approach ----
